29a9cf28bb90fa2e6eb0368954cc15162b4a0a37
Your Name <you@example.com>
2023-11-13 17:52:34 -0500

    j

--- src/unit.py
+++ src/unit.py
@@ -9,8 +9,9 @@ class Unit:
             self.ackt = stats[str(save)][str(name)]["Attack"]
             self.defn = stats[str(save)][str(name)]["Defence"]
             self.ackt = stats[str(save)][str(name)]["Attack"]
-            self.max_hp = stats[str(save)][str(name)]["Maximum Health Points"]
-            self.max_mp = stats[str(save)][str(name)]["Maximum Magic Points"]
+            self.hp = stats[str(save)][str(name)]["Maximum Health Points"]
+            self.mp = stats[str(save)][str(name)]["Maximum Magic Points"]
+
 
 
 class Enemy(Unit):
