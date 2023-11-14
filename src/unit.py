2d639fccb5802ff446f1327652fda55ed2c1151a
Your Name <you@example.com>
2023-11-14 15:07:00 -0500

    n

--- src/unit.py
+++ src/unit.py
@@ -9,11 +9,11 @@ class Unit:
             #r we using the json as a memory to load previous data through a username? I feel like that would be the best way to store it 
             # if I understand what you're saying, then yeah
             json.load(stats)
-            self.ackt = stats[str(save)][str(name)]["Attack"]
-            self.defn = stats[str(save)][str(name)]["Defense"]
-            self.ackt = stats[str(save)][str(name)]["Attack"]
-            self.max_hp = stats[str(save)][str(name)]["Maximum Health Points"]
-            self.max_mp = stats[str(save)][str(name)]["Maximum Magic Points"]
+            self.ackt = stats[str(save)]["Unit Stats"][str(name)]["Attack"]
+            self.defn = stats[str(save)]["Unit Stats"][str(name)]["Defense"]
+            self.ackt = stats[str(save)]["Unit Stats"][str(name)]["Attack"]
+            self.max_hp = stats[str(save)]["Unit Stats"][str(name)]["Maximum Health Points"]
+            self.max_mp = stats[str(save)]["Unit Stats"][str(name)]["Maximum Magic Points"]
             self.hp = self.max_hp
             self.mp = self.max_mp
 
@@ -29,4 +29,4 @@ class Hero(Unit):
     def __init__(self, save, name):
         with open("save_data.json") as stats:
             super().__init__(save, name)
-            self.exp = stats[str(save)][str(name)]["Experience Points"]
+            self.exp = stats[str(save)]["Unit Stats"][str(name)]["Experience Points"]
