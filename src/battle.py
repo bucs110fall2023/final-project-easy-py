473db8faab02dd62d95024811e7f8a752593b5db
Your Name <you@example.com>
2023-11-15 17:46:51 -0500

    f

--- src/battle.py
+++
@@ -1,8 +0,0 @@
-#from unit import Hero
-#from unit import Enemy
-
-#class BattleSequence:
-    
-#    def __init__(self, save, name):
-#        self.merant = Hero(save, "Merant")
-#        self.enemy = Enemy(save, name)
