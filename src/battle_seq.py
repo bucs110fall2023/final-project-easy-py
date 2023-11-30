51556518dfd69cecf61dcde924c2766d06cbd773
Your Name <you@example.com>
2023-11-30 08:42:17 -0500

    d

--- src/battle_seq.py
+++ src/battle_seq.py
@@ -3,11 +3,21 @@ from src.unit import *
 class BattleSeq:
 
     def __init__(self, save, name):
+        """
+        Initializes the classes that are going to be in the battle sequence
+        args: self, save, name
+        return: None
+        """
         self.merant = Hero(save, "Merant")
         self.enemy = Enemy(save, name)
         self.enemy_name = name
 
     def in_battle(self, input):
+        """
+        Follows the events of a battle (the main part of the game)
+        args: self input
+        return: None
+        """
         while not (self.merant.lose() and self.enemy.win()):
             if input == "1":
                 player_info = self.merant.attack(self.enemy_name)
---
+++ src/movable.py
@@ -0,0 +1,28 @@
+
+class OverworldUnit:
+
+    def __init__(self, x, y, length, width):
+        """
+        Initializes the information the player unit in the overworld
+        args: self, x, y, length, width
+        return: None
+        """
+        self.x = x
+        self.y = y
+        self.length = length
+        self.width = width
+
+    def move(self, input):
+        """
+        Allows the player unit to move in the overworld
+        args: self, input
+        return: None
+        """
+        if input == "UP":
+            self.y += 30
+        elif input == "DOWN":
+            self.y -= 30
+        elif input == "RIGHT":
+            self.x += 30
+        elif input == "LEFT":
+            self.x -= 30
