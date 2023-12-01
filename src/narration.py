8f60ff0ad175ba2a2729ba2ea7ea043bce4a0d78
Your Name <you@example.com>
2023-12-01 15:12:28 -0500

    f

--- src/narration.py
+++ src/narration.py
@@ -11,12 +11,24 @@ class ScrollingText:
 
     def scroll(self):
         with open(self.file, "r") as text:
+            story_lines = []
             for line in text.readlines():
-                story_lines = []
                 story_lines.append(line)
-            font = pygame.font.Font(None, self.width / 20)
+            font = pygame.font.Font(None, int(self.width / 20))
             y_pos = self.width
-            for scrolling_line in len(story_lines):
-                
-                font.render(scrolling_line, True, "white")
+            while True:    
+                for scrolling_line in story_lines:
+                    msg = font.render(scrolling_line, True, "white")
+                    self.screen.blit(msg, (10, y_pos))
+                    pygame.display.flip()
+                    y_pos -= 1
+                    self.screen.fill((0, 0, 0))
+
+def test():
+    pygame.init()
+    display = pygame.display.set_mode()
+    length, width = pygame.display.get_window_size()
+    test = ScrollingText(display, "story_beginning.txt", length, width)
+    test.scroll()
+test()
 
