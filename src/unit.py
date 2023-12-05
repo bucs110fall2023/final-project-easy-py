b8dec6077a1716f0baef2ff8e37b9fd0ae88a37b
Joshua Medina <josh.m@Joshuas-MacBook-Pro.local>
2023-12-05 15:32:47 -0500

    U

--- src/unit.py
+++ src/unit.py
@@ -118,21 +118,6 @@ class Hero(Unit):
             else:
                 return {}
 
-
-
-
-
-
-pygame.init()
-
-#game window
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 450
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Health Bar')
-
-
 class HealthBar:
     def __init__(self, x, y, w, h, max_hp):
         """
@@ -154,23 +139,3 @@ class HealthBar:
         ratio = self.hp / self.max_hp
         pygame.draw.rect(surface, "red", (self.x, self.y, self.w, self.h))
         pygame.draw.rect(surface, "green", (self.x, self.y, self.w * ratio, self.h))
-
-
-health_bar = HealthBar(250, 200, 300, 40, 100)
-
-run = True
-while run:
-
-  screen.fill("indigo")
-
-  #draw health bar
-  health_bar.hp = 50
-  health_bar.draw(screen)
-
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
-
-  pygame.display.flip()
-
-pygame.quit()
