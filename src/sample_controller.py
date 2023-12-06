d86e2cc918d11329585a95c4e67c2f38d1b4469f
Your Name <you@example.com>
2023-12-06 14:48:18 -0500

    mj

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -83,7 +83,7 @@ class Controller:
                             battle_info = battle_do.in_battle("1")
                             battle_eye.battle_dialogue(battle_do.get_hp(), None, battle_info["Player Narration"]["Enemy Damage"], battle_info["Player Narration"]["Opponent"])
                             battle_eye.battle_dialogue(battle_do.get_hp(), battle_info["Enemy Narration"]["Enemy Damage"])
-                        if event.type == pygame.K_2:
+                        elif event.type == pygame.K_2:
                             battle_info = battle_do.in_battle("2")
                             battle_eye.battle_dialogue(battle_do.get_hp(), battle_info["Enemy Narration"]["Enemy Damage"])
             elif battle_do.in_battle()["Loss"] == True:
