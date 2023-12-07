7e5055322f5dacb4693aa3c0f755d2502dd99407
Your Name <you@example.com>
2023-12-07 14:16:09 -0500

    d

--- src/save_prog.py
+++ src/save_prog.py
@@ -3,4 +3,12 @@ import json
 class SaveProg:
 
     def __init__(self, save):
-        pass
+        self.save = save
+
+    def save_update(self):
+        with open(r"../assets/save_data.json", "r") as save_read:    
+            save_data = json.load(save_read)
+        with open(r"../assets/save_data.json", "w") as save_add:
+            print(save_read)
+            save_data[self.save]["Progress"] += 1
+            json.dump(save_data, save_add, indent=4)
