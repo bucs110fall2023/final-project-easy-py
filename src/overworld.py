9cff3b1b867acc788f4837f54b3d183296e82f06
Your Name <you@example.com>
2023-12-07 16:16:41 -0500

    j

--- src/overworld.py
+++ src/overworld.py
@@ -14,7 +14,22 @@ class Overworld:
 
 
     def load_character(self):
-        self.hero = pygame.image.load(r"../final-project-easy-py/assets/Hero.png")
+        self.hero = pygame.image.load(r"../assets/Hero.png")
 
     def screen_jump(self, x, y):
-        self.screen.blit(self.hero, x, y, 50)
+        self.screen.blit(self.hero, (x, y))
+
+    def refresh(self):
+        pygame.display.flip()
+        self.screen.fill("black")
+
+def test():
+    pygame.init()
+    display = pygame.display.set_mode()
+    width, height = pygame.display.get_window_size()
+    explorer = Overworld(display, width, height)
+    explorer.load_character()
+    while True:
+        explorer.screen_jump(500, 500)
+        explorer.refresh()
+# test()
