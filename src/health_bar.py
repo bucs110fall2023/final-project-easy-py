995cf9e8d7c3edda61b380be303c17a38188dcba
Joshua Medina <josh.m@Joshuas-MacBook-Pro.local>
2023-12-07 21:43:01 -0500

    Final Commit

---
+++ src/health_bar.py
@@ -0,0 +1,23 @@
+import pygame
+class HealthBar:
+    def __init__(self, x, y, w, h, max_hp):
+        """
+        holds the x and y coordinates and the width and height of the healthbar
+        args: self, x, y, w, h, max_hp
+        return: None
+        """
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.max_hp = max_hp
+
+    def draw(self, surface):
+        """
+        draws the hero's health bar
+        args: self, surface
+        return: None
+        """
+        ratio = self.hp / self.max_hp
+        pygame.draw.rect(surface, "red", (self.x, self.y, self.w, self.h))
+        pygame.draw.rect(surface, "green", (self.x, self.y, self.w * ratio, self.h))
